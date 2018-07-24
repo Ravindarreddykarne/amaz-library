57f4d9f3f2a9335824b44dcf55fb1fd54bcb19d3
Ravindarreddykarne <rkavi421@gmail.com>
2018-07-24 15:08:24 +0530

    Add All Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  image1:string='./assets/amazon1.png'
+  image2:string='./assets/prime.PNG'
   title = 'app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
+
+const appRoutes:Routes = ([   
+  
+
+ {path:'login',component:LoginComponent},
+ {path:'dashboard',component:DashboardComponent}
+
+])
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
